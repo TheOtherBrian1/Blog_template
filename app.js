17140f2b3b094de84ad262dddc1324ff84857550
Brian Brennglass <bbrenng1@binghamton.edu>
2021-11-04 03:24:07 -0400

    fixed template, added CSS'

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const testData = require('./public/test_data');
 const app = express();
-const port = 3000;
+const port = 2000;
 app.set('view engine', 'pug');
 
-app.use(express.static('public'));
 app.get('/', (req, res) => {
-    console.log(testData);
-    //res.send('nothing');
+    console.log('req');
     res.render('index', testData)
+    console.log('rendered');
 })
+app.use(express.static('public'),()=>console.log('sc'));
+
+app.use((err, res, req, next)=>console.log(err));
   
 app.listen(port, () => console.log('listening', port));
